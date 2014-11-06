f65b93724cfbdf9e261e86c82b47677f82505c26
donsiu <spcdonsiu@gmail.com>
2014-11-05 23:56:31 -0600

    combine

---
+++ cloud/main.js
@@ -0,0 +1,7 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.initialize("wpInUaSnMuLflYrSMnv7c1SvNNuLbFoAT3S5Oby8","yLMJwfxDUcl5bi2O9Q7NngCYOxvraRUhq9eXySVB");
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Im hungry");
+});
