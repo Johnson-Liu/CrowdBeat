e91ee9e09a1ab747e7bac2bf4b3072b2925249d4
tedwu13 <tedwu2016@u.northwestern.edu>
2014-11-16 22:48:08 -0600

    added landing page

---
+++ public/js/DrawRectangle.js
@@ -0,0 +1,94 @@
+  var colorLoop = 0;
+  var colors = ["second1", "second2", "second3","second4", "second5","second6","second7","second8","second9","second10","second11","second12"];
+  var myDate = new Date();
+
+  colors[0] = ["#110014","#220029", "#33003D","#440052"];
+  //colors[0] = ["white","#220029", "#33003D","#440052"];
+  colors[1] = ["#33003D","#440052", "#520063","#66007A"];
+  colors[2] = ["#520063", "#66007A", "#77008F","#8800A3"];
+  colors[3] = ["#77008F", "#8800A3", "#9900B8","#AA00CC"];
+  colors[4] = ["#9900B8", "#AA00CC", "#BB00E0","#CC00F5"];
+  colors[5] = ["#BB00E0", "#CC00F5", "#D60AFF", "#DA1FFF"];
+  colors[6] = ["#D60AFF", "#DA1FFF", "#DD33FF","#E047FF"];
+  colors[7] = ["#DD33FF", "#E047FF","#E45CFF","#E770FF"];
+  colors[8] = ["#E45CFF", "#E770FF", "#EB85FF", "#EE99FF"];
+  colors[9] = ["#EB85FF", "#EE99FF", "#F1ADFF", "#F5C2FF"];
+  colors[10] = ["#F1ADFF", "#F5C2FF", "#F8D6FF", "#FCEBFF"];
+  //replace the white color
+  colors[11] = ["#F5C2FF", "#F8D6FF", "#FCEBFF","#FCEBFF"];
+
+  //#FFFFFF means white color
+  //#000000 means black color
+
+function main() { 
+  // alert(colors[0][0]);
+  var canvas = document.getElementById('example');  
+  if (!canvas) { 
+    console.log('Failed to retrieve the <canvas> element');
+    return false; 
+  } 
+  //alert("test1");
+  // Get the rendering context for 2DCG
+  var context = canvas.getContext('2d');
+  // s = myDate.getSeconds();
+  //alert(s);
+
+  // sleep(3000);
+  s =  myDate.getSeconds();
+  //alert(s2);
+
+  //let the code sleep until another new second comes
+  while(true){
+    var TempDate = new Date();
+    ms = TempDate.getMilliseconds();
+    // s = s+1;
+    if(ms == 0)
+      break;
+  }
+
+    s = s+1;
+    // there are six colors
+    r = s%colors.length;
+    //alert(colors.length);
+    colorLoop = r;
+    // alert(s);
+    //  every 1000ms, run the colorChange function
+    var interal = setInterval(function () {
+        colorChange(context,s);
+    }, 1000);
+}
+
+
+function colorChange(context, r) {
+  //clear all the screen
+   //give color to the paint
+   //context.fillStyle = "white";
+   //paint the background color
+   //context.fillRect(0, 0, 4000, 3000);
+   //give color to the paint
+   context.clearRect(0, 0, 1000, 1000);
+   var colorLength  =  colors[0].length - 1; 
+   var randomMark = getRandomInt(0 , colorLength);
+   context.fillStyle = colors[colorLoop][randomMark];
+   //draw the block in the middle
+   context.fillRect(0, 0, 1000, 1000);
+  colorLoop = colorLoop + 1;
+   if(colorLoop ==colors.length){
+    colorLoop =0;
+   }      
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function manualColorSet(){
+  //function to manually set color
+  // preload array of colors
+  // draw rectangle with certain color based on how much time has passed
+  // how do we get this to work for multiple users? 
+  //  should we make an admin page that sets variables and dates? 
+}
